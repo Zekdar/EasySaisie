9accc9d2bce26a1a98c21b14eff96e44bf6b6754
Zekdar <julien.hairapian@gmail.com>
2014-02-04 20:48:30 +0100

    Update vendors

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd73f1ce3c8cd7da716f96303e06d0c95::getLoader();
+return ComposerAutoloaderInit428ac31ed031931b622199715ac6c4c5::getLoader();
