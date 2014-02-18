962453d95c2c360489f5921f6edbe92f565e1213
JonnyBoet <jonathan.boet@gmail.com>
2014-02-18 22:22:36 +0100

    add FOSUserBundle

---
+++ src/C2J/UserBundle/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+// src/C2J/UserBundle/Entity/User.php
+
+namespace C2J\UserBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+  /**
+   * @ORM\Id
+   * @ORM\Column(type="integer")
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  protected $id;
+}
