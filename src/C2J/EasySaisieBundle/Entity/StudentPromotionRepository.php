caf88361f642e54bb03f8316e84d8b1157033e43
Zekdar <julien.hairapian@gmail.com>
2014-03-03 20:49:18 +0100

    StudentPromo repo change

--- src/C2J/EasySaisieBundle/Entity/StudentPromotionRepository.php
+++ src/C2J/EasySaisieBundle/Entity/StudentPromotionRepository.php
@@ -36,27 +36,61 @@ class StudentPromotionRepository extends EntityRepository
 
 	public function findAllMarksByStudentsByPromotion($promotion_id, $year) 
 	{
-		return $this->createQueryBuilder('sp')
-					->join('sp.student', 's')
-						->addSelect('s')
-					->join('sp.promotion', 'p')
-						->addSelect('p')
-					->leftJoin('sp.marks', 'm')
-						->addSelect('m')
-					->leftJoin('tus.marks', 'm')
-						->addSelect('tus')
-					->join('tus.subject', 'sub')
-						->addSelect('sub')
-					->join('tus.teachingUnit', 'tu')
-						->addSelect('tu')
-					->join('tu.container', 'c')
-						->addSelect('c')					
+		// return $this->createQueryBuilder('sp')
+		// 			->join('sp.student', 's')
+		// 				->addSelect('s')
+		// 			->join('sp.promotion', 'p')
+		// 				->addSelect('p')
+		// 			->leftJoin('sp.marks', 'm')
+		// 				->addSelect('m')
+		// 			->leftJoin('tus.marks', 'm')
+		// 				->addSelect('tus')
+		// 			->join('tus.subject', 'sub')
+		// 				->addSelect('sub')
+		// 			->join('tus.teachingUnit', 'tu')
+		// 				->addSelect('tu')
+		// 			->join('tu.container', 'c')
+		// 				->addSelect('c')					
 
-					->where('sp.promotion = :promotion_id')
-						->setParameter('promotion_id', $promotion_id)
-					->andWhere('sp.year = :year')
-						->setParameter('year', $year)
-					->getQuery()
-					->getResult();
+		// 			->where('sp.promotion = :promotion_id')
+		// 				->setParameter('promotion_id', $promotion_id)
+		// 			->andWhere('sp.year = :year')
+		// 				->setParameter('year', $year)
+		// 			->getQuery()
+		// 			->getResult();
+	 			
+		// $rsm = new ResultSetMapping();
+		// $rsm->addEntityResult('C2JEasySaisieBundle:StudentPromotion', 'sp');
+		// $rsm->addFieldResult('p', 'id', 'id');
+		// $rsm->addFieldResult('p', 'title', 'title');
+		 
+		// $sql = '
+		// 		SELECT 		*  
+		// 		FROM 		studentpromotion sp
+		// 		JOIN  		student s 				ON (sp.student_id = s.id)
+		// 		JOIN 		promotion p 			ON (sp.promotion_id = p.id)
+		// 		LEFT JOIN 	mark m 					ON (sp.id = m.studentpromotion_id)
+		// 		LEFT JOIN 	teachingunitsubject tus ON (tus.id = m.teachingunitsubject_id)
+		// 		JOIN 		subject sub 			ON (sub.id = tus.subject_id)
+		// 		JOIN 		teachingunit tu 		ON (tu.id = tus.teachingunit_id)
+		// 		JOIN 		container c 			ON (c.id = tu.container_id)
+				
+		// 		UNION 		
+ 
+		// 		SELECT 		*  
+		// 		FROM 		studentpromotion sp
+		// 		JOIN  		student s 				ON (sp.student_id = s.id)
+		// 		JOIN 		promotion p 			ON (sp.promotion_id = p.id)
+		// 		RIGHT JOIN 	mark m 					ON (sp.id = m.studentpromotion_id)
+		// 		RIGHT JOIN 	teachingunitsubject tus ON (tus.id = m.teachingunitsubject_id)
+		// 		JOIN 		subject sub 			ON (sub.id = tus.subject_id)
+		// 		JOIN 		teachingunit tu 		ON (tu.id = tus.teachingunit_id)
+		// 		JOIN 		container c 			ON (c.id = tu.container_id)
+		// ';
+		 
+		// $query = $this->_em->createNativeQuery($sql, $rsm);
+		// $query->setParameter(1, $idProject);
+		 
+		// $projects = $query->getResult();
 	}
 }
